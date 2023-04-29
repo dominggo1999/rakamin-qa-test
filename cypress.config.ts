d8608d057265a900b939ab144b53a0972395f97e
arnold-simanjuntak <arnolddominggo19@gmail.com>
2023-04-30 00:47:49 +0700

    feat : add gherkin syntax

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "cypress";
+import cucumberPreprocessor from "cypress-cucumber-preprocessor";
+import browserify from "@cypress/browserify-preprocessor";
 
 export default defineConfig({
   reporter: "cypress-mochawesome-reporter",
@@ -11,9 +13,21 @@ export default defineConfig({
     saveJson: true,
   },
   e2e: {
-    setupNodeEvents(on) {
+    specPattern: "**/*.feature",
+    setupNodeEvents(on, config) {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       require("cypress-mochawesome-reporter/plugin")(on);
+
+      const options: browserify.Options = {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        ...require("@cypress/browserify-preprocessor").defaultOptions,
+        typescript: require.resolve("typescript"),
+      };
+
+      on("file:preprocessor", cucumberPreprocessor(options));
+
+      // Make sure to return the config object as it might have been modified by the plugin.
+      return config;
     },
     baseUrl: "https://web-staging.rakamin.com",
   },
--- cypress/e2e/hello.cy.ts
+++
@@ -1,9 +0,0 @@
-import { ExamplePage } from "page-objects";
-
-describe("Hello", () => {
-  it("world", () => {
-    expect(true).to.be.true;
-    cy.getByDataCy("test");
-    ExamplePage.visit();
-  });
-});
