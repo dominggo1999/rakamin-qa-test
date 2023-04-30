e1ce10a2f69b9310f2824f3c791d2f3f7f3f38de
arnold-simanjuntak <arnolddominggo19@gmail.com>
2023-04-30 12:31:41 +0700

    feat : add assertUrlEquals command

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -4,11 +4,17 @@ Cypress.Commands.add("getByDataCy", (value) => {
   return cy.get(`[data-cy=${value}]`);
 });
 
+Cypress.Commands.add("assertUrlEquals", (path) => {
+  const baseUrl = Cypress.config("baseUrl");
+  cy.url().should("equal", `${baseUrl}${path}`);
+});
+
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
       getByDataCy(value: string): Chainable<JQuery<HTMLElement>>;
+      assertUrlEquals(path: string): void;
     }
   }
 }
